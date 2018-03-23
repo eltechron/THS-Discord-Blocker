f3eec3b6693a57044f61e79bab8b582263641e54
Joshua Downes <eltechron@gmail.com>
2018-03-23 14:02:40 +1100

    I done a oops

--- Service/THS Discord Blocker/service.cs
+++ Service/THS Discord Blocker/service.cs
@@ -43,7 +43,7 @@ namespace THS_Discord_Blocker
             }
 
             //Detect if connected to Heights network
-            if (!connectedSsids.Contains("THS-Students") || connectedSsids.Contains("THS-Secure") || connectedSsids.Contains("Achilleus"))
+            if (connectedSsids.Contains("THS-Students") || connectedSsids.Contains("THS-Secure") || connectedSsids.Contains("Achilleus"))
             {
                 NotifyIcon.ni_notify(5000, "Heights Network Detected", "Discord will not open.", ToolTipIcon.Info);
                 //Block Discord if already open?
